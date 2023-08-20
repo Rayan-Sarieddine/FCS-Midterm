239c5b92e4b11eb6bf7495a1793f58f52c4305b7
Rayan sarieddine <rayan2.sarieddine@gmail.com>
2023-08-20 16:08:48 +0300

    added statistics function and updated read employees function

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import datetime
 employees={}
 
+#read employees line by line from users.txt file
 def readEmployees(employees):#time complexity=O(n) where n= number of lines in the users file
-    with open("test2.txt", "r") as f: #https://www.w3schools.com/python/python_file_open.asp
+    with open("users.txt", "r") as f: #https://www.w3schools.com/python/python_file_open.asp
       for x in f:
           line=x.split(",")
           employees[line[0].strip().lower()]={
@@ -11,3 +12,15 @@ def readEmployees(employees):#time complexity=O(n) where n= number of lines in t
           "gender":line[3].strip().lower(),
           "salary":float(line[4].strip())
         }
+          
+
+#display number of male and female employees registered in the system
+def statistics(employees):# time complexity: O(n) where n= number of employees (i.e number of lines in the users file)
+    countMale=0
+    countFemale=0
+    for emp in employees.values():
+        if emp['gender'] == 'male':
+            countMale=+1
+        elif emp['gender'] == 'female':
+           countFemale+=1
+    return f"there are {countFemale} female employees and {countMale} male employees"
